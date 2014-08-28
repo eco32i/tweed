1e8ca9df24c0bb21256920e324e67a08338213c7
Ilya Shamovsky <ilya.shamovsky@gmail.com>
2014-08-28 10:09:47 -0400

    Fixed keygen.py

--- tweed/keygen.py
+++ tweed/keygen.py
@@ -1,5 +1,7 @@
 from django.utils.crypto import get_random_string
 
 chars = '1234567890-=!@#$%^&*()_+qwertyuiopasdfghjkl;zxcvbnm,./'
-with open('secret_key.py', 'w') as keyfile:
-    keyfile.write('SECRET_KEY = %s' % get_random_string(50, chars))
+
+def generate_secret_key(filepath):
+    with open(filepath,  'w') as keyfile:
+        keyfile.write('SECRET_KEY = %s' % get_random_string(50, chars))
